d8dac5b5e6eab159b6aa1a4b8d7a85ec83eb5f2e
dariomontani <dariomontani@gmail.com>
2022-01-10 17:19:38 +0100

    stampo in pagina 10 email

--- js/script.js
+++ js/script.js
@@ -6,22 +6,22 @@
 const app = new Vue({
     el: '#app',
     data: {
-        emails: [null],
+        emails: [],
     },
 
     created(){
-    for (let index = 0; index < 10; index++) {
-        axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((response) => {
-                console.log(response.data.response);
-                this.emails = response.data.response;
-                
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    
-    }
+        for (let index = 0; index < 10; index++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    console.log(response.data.response);
+                    this.emails.push(response.data.response);
+                    
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        
+        }
     }
 })
